72c406f1377eb0e02f0708066fd41b3b9ba9da3c
ignaciobatalla <90469702+ignaciobatalla@users.noreply.github.com>
2022-03-12 00:26:07 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './estilo.css';
+import Navbar from './component/NavBar.js';
+
+function App() {
+  return (
+    <Navbar/>
+  );
+}
+
+export default App;
---
+++ src/component/NavBar.js
@@ -0,0 +1,17 @@
+import react from 'react'
+import '../estilo.css';
+const Navbar = ()=>{
+    return(
+        <header class ='header'>
+            <a href='#'>HOME</a>
+            <nav class='nav'>
+                <a href='#'>NOSOTROS</a>
+                <a href='#'>SERVICIOS</a>
+                <a href='#'>CONTACTO</a>
+            </nav>
+
+        </header>
+
+    )
+}
+export default Navbar
